bf29b46a39fd966018543875ce34e7cca07655a9
Jayasawar <rautjs@gmail.com>
2025-04-02 22:26:27 +0545

    add task and user models with validation and schema definitions

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose,{Schema} from 'mongoose'
+const userSchema = new Schema({
+name:{
+    type:String,
+    required:[true,'Username is required'],
+    minLength:5
+},
+email:{
+    type:String,
+    required:[true,'Email is required'],
+    lowercase:true,
+    unique:true,
+    match:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+},
+password:{
+    type:String,
+    required:[true,'Password is required'],
+    minLength:6
+}
+},{timestamps:true})
+export const User = mongoose.model('User',userSchema)
